477611543280cb5236ad5ca13b63a05380ccf97c
mjs <stephure@users.noreply.github.com>
2016-10-26 14:56:34 -0400

    Update build.bat
    Use ELK v5

--- build.bat
+++ build.bat
@@ -4,16 +4,16 @@ SET NSSM=tools\nssm\win64\nssm.exe
 SET ZIP=tools\7zip\7za.exe
 
 SET VERSION=1.0.1
-SET ELASTIC_SEARCH_VERSION=2.3.1
-SET LOGSTASH_VERSION=2.3.1
-SET KIBANA_VERSION=4.5.0
+SET ELASTIC_SEARCH_VERSION=5.0.0
+SET LOGSTASH_VERSION=5.0.0
+SET KIBANA_VERSION=5.0.0
 
 rem ---------- Download packages ----------
 if not exist "downloads" mkdir downloads
 
-if not exist "downloads\elasticsearch-%ELASTIC_SEARCH_VERSION%.zip" %CURL% "https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/zip/elasticsearch/%ELASTIC_SEARCH_VERSION%/elasticsearch-%ELASTIC_SEARCH_VERSION%.zip" -o downloads\elasticsearch-%ELASTIC_SEARCH_VERSION%.zip
-if not exist "downloads\logstash-%LOGSTASH_VERSION%.zip" %CURL% "https://download.elastic.co/logstash/logstash/logstash-%LOGSTASH_VERSION%.zip" -o downloads\logstash-%LOGSTASH_VERSION%.zip
-if not exist "downloads\kibana-%KIBANA_VERSION%.zip" %CURL% "https://download.elastic.co/kibana/kibana/kibana-%KIBANA_VERSION%-windows.zip" -o downloads\kibana-%KIBANA_VERSION%.zip
+if not exist "downloads\elasticsearch-%ELASTIC_SEARCH_VERSION%.zip" %CURL% "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-%ELASTIC_SEARCH_VERSION%.zip" -o downloads\elasticsearch-%ELASTIC_SEARCH_VERSION%.zip
+if not exist "downloads\logstash-%LOGSTASH_VERSION%.zip" %CURL% "https://artifacts.elastic.co/downloads/logstash/logstash-%LOGSTASH_VERSION%.zip" -o downloads\logstash-%LOGSTASH_VERSION%.zip
+if not exist "downloads\kibana-%KIBANA_VERSION%.zip" %CURL% "https://artifacts.elastic.co/downloads/kibana/kibana-%KIBANA_VERSION%-windows-x86.zip" -o downloads\kibana-%KIBANA_VERSION%.zip
 rem --------------------------------------
 
 rem ---------- Unzip packages ----------
